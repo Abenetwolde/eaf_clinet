5493a1f43806304ad61f69fc505246bccda10604
Abenetwolde <abnetwoldedev@gmail.com>
2026-08-24 06:34:45 +0000

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import { LanguageProvider } from './i18n'
 import { store } from './store/store'
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <LanguageProvider>
         <App />
+        <Analytics />
       </LanguageProvider>
     </Provider>
   </StrictMode>,
